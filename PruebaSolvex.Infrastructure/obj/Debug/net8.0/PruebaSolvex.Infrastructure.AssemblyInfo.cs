881182ddb969f22f1c115670b7200340a69ba95d
Daurin Gonzalez <dauringonzales6@gmail.com>
2025-05-14 22:11:29 -0400

    Actualizand campo Rol y el Readme

--- PruebaSolvex.Infrastructure/obj/Debug/net8.0/PruebaSolvex.Infrastructure.AssemblyInfo.cs
+++ PruebaSolvex.Infrastructure/obj/Debug/net8.0/PruebaSolvex.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaSolvex.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c5a11430c83787f9f8d4b209783295279dd45c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b0186c884cc2209800f49c7e2dd937ebd94003d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaSolvex.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaSolvex.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
